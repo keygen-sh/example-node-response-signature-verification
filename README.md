a0599740901037359ffddf2fe7428a8aa50748af
Zeke Gabrielse <ezekg@yahoo.com>
2021-06-08 17:09:52 -0500

    update example to verify ed25519 signatures

--- README.md
+++ README.md
@@ -1,30 +1,27 @@
 # Example Signature Verification
-This is an example of [verifying signatures](https://keygen.sh/docs/api#signatures)
-using your Keygen account's public key. You can find your public key
+This is an example of [verifying response signatures](https://keygen.sh/docs/api#signatures)
+using your Keygen account's unique Ed25519 public key. You can find your public keys
 within [your account's settings page](https://app.keygen.sh/settings).
-Verifying response signatures will help prevent simple MITM attacks,
-where the attacker redirects traffic from your licensing server
-(e.g. Keygen) to their own locally controlled server, or where you
-have cached a validation response locally and want to verify its
-integrity (i.e. it has not been tampered with).
+
+Verifying response signatures will help prevent man-in-the-middle and replay
+attacks, where the attacker redirects traffic from your licensing server
+(e.g. Keygen) to their own locally controlled server. Other examples are
+when you have cached an API response locally and want to verify its integrity
+(i.e. it has not been tampered with).
 
 ## Running the example
 
 First up, configure a few environment variables:
+
 ```bash
+# Your Keygen account's Ed25519 verify key
+export KEYGEN_VERIFY_KEY="YOUR_KEYGEN_ED25519_VERIFY_KEY"
+
 # Keygen product token (don't share this!)
 export KEYGEN_PRODUCT_TOKEN="YOUR_KEYGEN_PRODUCT_TOKEN"
 
 # Your Keygen account ID
 export KEYGEN_ACCOUNT_ID="YOUR_KEYGEN_ACCOUNT_ID"
-
-# Your Keygen account's public key
-export KEYGEN_PUBLIC_KEY=$(printf %b \
-  '-----BEGIN PUBLIC KEY-----\n' \
-  'zdL8BgMFM7p7+FGEGuH1I0KBaMcB/RZZSUu4yTBMu0pJw2EWzr3CrOOiXQI3+6bA\n' \
-  # …
-  'efK41Ml6OwZB3tchqGmpuAsCEwEAaQ==\n' \
-  '-----END PUBLIC KEY-----')
 ```
 
 You can either run each line above within your terminal session before
@@ -32,11 +29,13 @@ starting the app, or you can add the above contents to your `~/.bashrc`
 file and then run `source ~/.bashrc` after saving the file.
 
 Next, install dependencies with [`yarn`](https://yarnpkg.comg):
+
 ```
 yarn
 ```
 
 Then run the script with the route you want to fetch:
+
 ```
 yarn start '/licenses/442160c6-20d2-44a7-883d-245e38f651fd'
 yarn start '/users/dbe63060-eee7-4c87-98fa-f133fb8131fa'
--- index.js
+++ index.js
@@ -1,57 +1,158 @@
 const {
+  KEYGEN_VERIFY_KEY,
   KEYGEN_PRODUCT_TOKEN,
   KEYGEN_ACCOUNT_ID,
-  KEYGEN_PUBLIC_KEY
 } = process.env
 
 const fetch = require('node-fetch')
 const crypto = require('crypto')
 const chalk = require('chalk')
 
+// There is likely a third-party module for this, but we want to show
+// how to parse the signature header without one.
+function parseParameterizedHeader(header) {
+  if (header == null) {
+    return null
+  }
+
+  const params = header.split(/\s*,\s*/g)
+  const keyvalues = params.map(param => {
+    const [, key, value] = param.match(/([^=]+)="([^"]+)"/i)
+
+    return [key, value]
+  })
+
+  return keyvalues.reduce(
+    (o, [k, v]) => (o[k] = v, o),
+    {}
+  )
+}
+
+// Encode hexadecimal key into DER format, since Node's crypto module doesn't
+// accept keys in hex format. This is all a bit magical, but it creates a
+// buffer of bytes according to the DER spec. There are likely third-party
+// modules for this.
+function encodeHexKeyToDerFormat(hex) {
+  const oid = Buffer.from([0x06, 0x03, 0x2B, 0x65, 0x70]) // Ed25519 oid
+  const key = Buffer.from(hex, 'hex')
+  const elements = Buffer.concat([
+    Buffer.concat([
+      Buffer.from([0x30]), // Sequence tag
+      Buffer.from([oid.length]),
+      oid,
+    ]),
+    Buffer.concat([
+      Buffer.from([0x03]), // Bit tag
+      Buffer.from([key.length + 1]),
+      Buffer.from([0x00]), // Zero bit
+      key,
+    ]),
+  ])
+
+  const der = Buffer.concat([
+    Buffer.from([0x30]), // Sequence tag
+    Buffer.from([elements.length]),
+    elements,
+  ])
+
+  return der
+}
+
 async function main() {
   const route = process.argv[2] || ''
-
-  const res = await fetch(`https://api.keygen.sh/v1/accounts/${KEYGEN_ACCOUNT_ID}/${route}`, {
+  const uri = `/v1/accounts/${KEYGEN_ACCOUNT_ID}/${route.replace(/^\//, '')}`
+  const res = await fetch(`https://api.keygen.sh/${uri.replace(/^\//, '')}`, {
+    method: 'GET',
     headers: {
       'Authorization': `Bearer ${KEYGEN_PRODUCT_TOKEN}`,
-      'Accept': 'application/vnd.api+json'
+      'Accept': 'application/vnd.api+json',
     }
   })
 
-  // Get the response body so we can use it for validating the signature
+  // Get the response body so we can use it for verifying its signature
   const body = await res.text()
 
-  const { meta, data, links, errors } = JSON.parse(body)
-  if (errors) {
-    console.error(
-      chalk.red(JSON.stringify({ errors }, null, 2))
-    )
-
-    return
-  }
-
-  // Validate the signature of the response using our public key
-  const sig = res.headers.get('X-Signature')
+  // Parse and verify the response signature
   try {
-    const verifier = crypto.createVerify('sha256')
-    verifier.write(body)
-    verifier.end()
+    // Parse the signature header
+    const header = parseParameterizedHeader(res.headers.get('Keygen-Signature'))
+    if (header == null) {
+      console.error(
+        chalk.red(`Signature is missing`)
+      )
 
-    const v = verifier.verify(KEYGEN_PUBLIC_KEY, sig, 'base64')
-    if (!v) {
-      throw new Error('Signature does not match')
+      process.exit(1)
+    }
+
+    // Extract the algorithm and signature from the header
+    const { algorithm, signature } = header
+
+    // Ensure signing algorithm is what we expect
+    if (algorithm !== 'ed25519') {
+      console.error(
+        chalk.red(`Algorithm did not match: ${algorithm}`)
+      )
+
+      process.exit(1)
+    }
+
+    // Verify integrity
+    const hash = crypto.createHash('sha256').update(body)
+    const digest = `sha-256=${hash.digest('base64')}`
+    if (digest !== res.headers.get('Digest')) {
+      console.error(
+        chalk.red(`Digest did not match: ${digest}`)
+      )
+
+      process.exit(1)
+    }
+
+    // Reconstruct the signing data
+    const date = res.headers.get('Date')
+    const data = [
+      `(request-target): get ${uri}`,
+      `host: api.keygen.sh`,
+      `date: ${date}`,
+      `digest: ${digest}`,
+    ].join('\n')
+
+    // Initialize our public key
+    const verifyKey = crypto.createPublicKey({
+      key: encodeHexKeyToDerFormat(KEYGEN_VERIFY_KEY),
+      format: 'der',
+      type: 'spki',
+    })
+
+    // Decode and verify the signature
+    const signatureBytes = Buffer.from(signature, 'base64')
+    const dataBytes = Buffer.from(data)
+    const ok = crypto.verify(null, dataBytes, verifyKey, signatureBytes)
+    if (!ok) {
+      throw new Error('invalid signature')
     }
   } catch (e) {
     console.error(
-      chalk.red(`Signature did not match: ${sig}`)
+      chalk.red(`Signature did not match: ${e.message}`)
     )
 
-    return
+    process.exit(1)
+  }
+
+  // Handle request normally
+  const { meta, data, links, errors } = JSON.parse(body)
+  if (errors) {
+    console.warn(
+      chalk.yellow(JSON.stringify({ errors }, null, 2))
+    )
+
+    process.exit(1)
   }
 
   console.log(
     chalk.green(JSON.stringify({ meta, data, links }, null, 2))
   )
+
+  process.exit(0)
 }
 
 main()
